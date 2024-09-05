c83727f9ee1c495d91267626b303ffd9dc019c41
CRISPpp <2715554797@qq.com>
2024-09-06 00:27:08 +0800

    feat: add dhcp config for hotspot

--- hotspot/create_hotspot_5GHz.sh
+++ hotspot/create_hotspot_5GHz.sh
@@ -2,6 +2,6 @@
 
 source hotspot.conf
 nmcli connection add type wifi ifname $IFACE1 con-name $HOTSPOT_NAME autoconnect no ssid $HOTSPOT_SSID
-nmcli connection modify $HOTSPOT_NAME 802-11-wireless.mode ap 802-11-wireless.band a ipv4.method shared
+nmcli connection modify $HOTSPOT_NAME 802-11-wireless.mode ap 802-11-wireless.band a ipv4.addresses 192.168.100.1/24 ipv4.method shared
 nmcli connection modify $HOTSPOT_NAME 802-11-wireless-security.key-mgmt wpa-psk 802-11-wireless-security.psk $HOTSPOT_PASSWORD
 nmcli connection up $HOTSPOT_NAME
